ded336c872b985e051dbea3c9531c5f1b0fd8368
VictorLosser <victor.losser@viacesi.fr>
2016-12-15 09:24:48 +0100

    Add files via upload

--- stats.h
+++ stats.h
@@ -3,10 +3,13 @@ struct Input{
     char date[11];
     char heure[11];
     char type[6];
-    char parametre[20]; //le nom de nos fichier ne depassera pas les 20 caractere et les dimensions non plus
+    char parametre[20]; //Le nom de nos fichiers ne depassera pas les 20 caracteres et les dimensions non plus
 };
+
+int nombreLignes(void);
 void ReadStats(Input input[]);
 void triDate(Input input[]);
 void triHeure(Input input[]);
 void PrintStats(Input input[]);
-void Counter(Input input[]);
+void Type_tS(Input input[]);
+void Image_tS1_lu(void);
